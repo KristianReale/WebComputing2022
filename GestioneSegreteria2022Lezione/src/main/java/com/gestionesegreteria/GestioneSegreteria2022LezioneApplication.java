187229eb15b7584efada1940f123addabc35f2a6
KristianReale <reale@mat.unical.it>
2021-10-21 11:49:25 +0200

    Modificato main

--- GestioneSegreteria2022Lezione/src/main/java/com/gestionesegreteria/GestioneSegreteria2022LezioneApplication.java
+++ GestioneSegreteria2022Lezione/src/main/java/com/gestionesegreteria/GestioneSegreteria2022LezioneApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class GestioneSegreteria2022LezioneApplication {
 
 	public static void main(String[] args) {
+		
 		SpringApplication.run(GestioneSegreteria2022LezioneApplication.class, args);
 	}
 
