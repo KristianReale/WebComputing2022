5172d1722330d6a00b714b264b0acc9f3058b772
KristianReale <reale@mat.unical.it>
2021-11-11 12:35:00 +0100

    Introdotto Javascript

---
+++ GestioneSegreteria2022Lezione/src/main/resources/static/js/servizi.js
@@ -0,0 +1,59 @@
+//alert("Servizi Studenti");
+
+//ISEE
+//Numero componenti nucleo familiare
+//Reddito dei componenti
+//Patrimonio dei componenti
+
+numeroComponenti = prompt("Inserisci il numero dei componenti");
+
+redditi = new Array();
+patrimoni = new Array();
+
+for (var i = 0; i < numeroComponenti; i++){
+	redditi[i] = prompt("Inserisci il reddito del componente " + (i + 1) );
+	patrimoni[i] = prompt("Inserisci il patrimonio del componente " + (i + 1) );
+} 
+
+ISR = 0;
+ISP = 0;
+riepilogo = "";
+for (var i = 0; i < numeroComponenti; i++){
+	riepilogo += "Reddito " + (i + 1) + ": " + redditi[i] + "\n";
+	riepilogo += "Patrimonio " + (i + 1) + ": " + patrimoni[i] + "\n";
+	
+	ISR += parseInt(redditi[i]);
+	ISP += parseInt(patrimoni[i]);
+}
+
+ISE = ISR + ISP * 20/100;
+
+console.log(riepilogo);
+console.log("ISP " + ISP);
+console.log("ISR " + ISR);
+console.log("ISE " + ISE);
+
+// 1 -> 1
+// 2 -> 1.57
+// 3 -> 2.04
+// 4 -> 2.46
+// >5 -> 2.85
+
+scalaEquivalenza = {
+	1: 1,
+	2: 1.57,
+	3: 2.04,
+	4: 2.46,
+	me5: 2.85
+};
+
+if (numeroComponenti < 5){
+	SE = scalaEquivalenza[numeroComponenti];
+}else{
+	SE = scalaEquivalenza["me5"];
+}
+console.log("SE " + SE);
+
+ISEE = ISE / SE;
+console.log("ISEE " + ISEE);
+alert("ISEE " + ISEE);
