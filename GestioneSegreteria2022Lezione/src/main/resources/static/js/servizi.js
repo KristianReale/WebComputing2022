4813c40b1c5f9c188d971333d67041fde5c6e876
KristianReale <reale@mat.unical.it>
2021-11-11 13:30:15 +0100

    Funzioni e oggetti con javascript

--- GestioneSegreteria2022Lezione/src/main/resources/static/js/servizi.js
+++ GestioneSegreteria2022Lezione/src/main/resources/static/js/servizi.js
@@ -1,59 +1,105 @@
 //alert("Servizi Studenti");
 
-//ISEE
-//Numero componenti nucleo familiare
-//Reddito dei componenti
-//Patrimonio dei componenti
+function Studente(matricola, cognome, nome, numeroComponenti){
+	this.matricola = matricola;
+	this.cognome = cognome;
+	this.nome = nome;
+	this.numeroComponenti = numeroComponenti;
 
-numeroComponenti = prompt("Inserisci il numero dei componenti");
+	this.dammiInfo = function(){
+		return this.matricola + " " + this.nome + " " + this.cognome;
+	}
+}
 
-redditi = new Array();
-patrimoni = new Array();
+/*
+elencoStudenti = new Array();
+elencoStudenti.push(new Studente("123456", "Rossi", "Mario"));
+elencoStudenti.push(new Studente("123321", "Bianchi", "Bruno"));
+elencoStudenti.push(new Studente("132111", "Verdi", "Giulia"));
+*/
 
-for (var i = 0; i < numeroComponenti; i++){
-	redditi[i] = prompt("Inserisci il reddito del componente " + (i + 1) );
-	patrimoni[i] = prompt("Inserisci il patrimonio del componente " + (i + 1) );
-} 
+studentiConId = {
+	"123456": new Studente("123456", "Rossi", "Mario"),
+	"123321": new Studente("123321", "Bianchi", "Bruno"),
+	"132111": new Studente("132111", "Verdi", "Giulia")
+}
 
-ISR = 0;
-ISP = 0;
-riepilogo = "";
-for (var i = 0; i < numeroComponenti; i++){
-	riepilogo += "Reddito " + (i + 1) + ": " + redditi[i] + "\n";
-	riepilogo += "Patrimonio " + (i + 1) + ": " + patrimoni[i] + "\n";
+function prendiStudente(matricola){
+	return studentiConId[matricola];
+}
+
+
+function prendiMatricolaPassata(){
+	urlStringa = window.location;
+	url = new URL(urlStringa);
+	var matricola = url.searchParams.get("matricola");
+	return matricola;
+}
+
+function calcolaISEE(){
+	console.log(prendiMatricolaPassata());
+	studente = prendiStudente(prendiMatricolaPassata());
+	alert(studente.dammiInfo());
+	//ISEE
+	//Numero componenti nucleo familiare
+	//Reddito dei componenti
+	//Patrimonio dei componenti
 	
-	ISR += parseInt(redditi[i]);
-	ISP += parseInt(patrimoni[i]);
+	if(typeof studente.numeroComponenti === 'undefined') {	
+		studente.numeroComponenti = prompt("Inserisci il numero dei componenti");
+	}else{
+		alert("Numero componenti: " + studente.numeroComponenti);
+	}
+	
+	redditi = new Array();
+	patrimoni = new Array();
+	
+	for (var i = 0; i < studente.numeroComponenti; i++){
+		redditi[i] = prompt("Inserisci il reddito del componente " + (i + 1) );
+		patrimoni[i] = prompt("Inserisci il patrimonio del componente " + (i + 1) );
+	} 
+	
+	ISR = 0;
+	ISP = 0;
+	riepilogo = "";
+	for (var i = 0; i < studente.numeroComponenti; i++){
+		riepilogo += "Reddito " + (i + 1) + ": " + redditi[i] + "\n";
+		riepilogo += "Patrimonio " + (i + 1) + ": " + patrimoni[i] + "\n";
+		
+		ISR += parseInt(redditi[i]);
+		ISP += parseInt(patrimoni[i]);
+	}
+	
+	ISE = ISR + ISP * 20/100;
+	
+	console.log(riepilogo);
+	console.log("ISP " + ISP);
+	console.log("ISR " + ISR);
+	console.log("ISE " + ISE);
+	
+	// 1 -> 1
+	// 2 -> 1.57
+	// 3 -> 2.04
+	// 4 -> 2.46
+	// >5 -> 2.85
+	
+	scalaEquivalenza = {
+		1: 1,
+		2: 1.57,
+		3: 2.04,
+		4: 2.46,
+		me5: 2.85
+	};
+	
+	if (numeroComponenti < 5){
+		SE = scalaEquivalenza[numeroComponenti];
+	}else{
+		SE = scalaEquivalenza["me5"];
+	}
+	console.log("SE " + SE);
+	
+	ISEE = ISE / SE;
+	console.log("ISEE " + ISEE);
+	return ISEE;
 }
 
-ISE = ISR + ISP * 20/100;
-
-console.log(riepilogo);
-console.log("ISP " + ISP);
-console.log("ISR " + ISR);
-console.log("ISE " + ISE);
-
-// 1 -> 1
-// 2 -> 1.57
-// 3 -> 2.04
-// 4 -> 2.46
-// >5 -> 2.85
-
-scalaEquivalenza = {
-	1: 1,
-	2: 1.57,
-	3: 2.04,
-	4: 2.46,
-	me5: 2.85
-};
-
-if (numeroComponenti < 5){
-	SE = scalaEquivalenza[numeroComponenti];
-}else{
-	SE = scalaEquivalenza["me5"];
-}
-console.log("SE " + SE);
-
-ISEE = ISE / SE;
-console.log("ISEE " + ISEE);
-alert("ISEE " + ISEE);
